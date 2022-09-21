cdaaf1d96e5d9cb2e02f8f7a934230c4ef8b0f17
aba097 <socceraba2701@gmail.com>
2022-09-21 18:27:12 +0900

    add: ViewとViewControllerの接続

--- BookManager/LoginViewController.swift
+++ BookManager/LoginViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userSelectionPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userSelectionPickerView.dataSource = self
+        userSelectionPickerView.delegate = self
+        
+        
     }
 
 
+    @IBAction func pressedLoginButton(_ sender: Any) {
+    }
+    
+}
+
+extension LoginViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    //pickerviewの列数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //pickerviewの行数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        2
+    }
+    
+    //pickerviewの各データ
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "test"
+    }
+    
+    
+   /*
+   // pickerview選択時
+   func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       self.textField.text = list[row]
+   }
+    */
 }
 
