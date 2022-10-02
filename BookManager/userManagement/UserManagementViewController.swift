fc8cfb0887a6fd3a555a6a3d8262db7cbb2fabaf
aba097 <socceraba2701@gmail.com>
2022-10-02 15:56:21 +0900

    add : userManagementのcontrollerとViewの接続

--- BookManager/userManagement/UserManagementViewController.swift
+++ BookManager/userManagement/UserManagementViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class UserManagementViewController: UIViewController {
 
+    @IBOutlet weak var inputAddUsernameTextField: UITextField!
+    
+    @IBOutlet weak var userSelectionPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func pressedUserAdditionalButton(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func pressedUserDeletingButton(_ sender: Any) {
     }
-    */
-
+    
 }
